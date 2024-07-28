2d2e9fce24aeb75b96d49064dc55bfe007567d50
Kavishka Gayanjalee <kgayanjalee@gmail.com>
2024-07-28 17:30:03 +0000

    Serve JSON on a Specific Route

--- myApp/myApp.js
+++ myApp/myApp.js
@@ -11,6 +11,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
+// Route to serve JSON response
+app.get('/json', (req, res) => {
+    res.json({ message: "Hello json" });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
