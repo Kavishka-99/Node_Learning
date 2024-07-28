123b4b976e2d6d626fb4fa853c884155b4b8e5a4
Kavishka Gayanjalee <kgayanjalee@gmail.com>
2024-07-28 18:15:24 +0000

    Chain Middleware to Create a Time Server

--- myApp/myApp.js
+++ myApp/myApp.js
@@ -28,6 +28,14 @@ app.get('/json', (req, res) => {
     res.json({ message: message });
 });
 
+// Route to get current time
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({ time: req.time });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
